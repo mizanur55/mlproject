905d231005645db86f028044927120bcdf8dc6aa
Mizanur Rahman <mizanur55@hotmail.com>
2023-08-31 10:58:59 +0100

    added new files

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace("/", " ") for req in requirements]
+        requirements = [req.replace("/", "") for req in requirements]
 
     if HYPHEN_E_DOT in requirements:
         requirements.remove(HYPHEN_E_DOT)
@@ -21,14 +21,14 @@ def get_requirements(file_path: str) -> List[str]:
 
 # write the metadata about the entire project
 setup(
-name = "mlproject"
-version = '0.0.1'
-author = 'Mizan'
-author_email = "mizanur55@hotmail.com"
-# this will install all the packages in from every folder with __init__ 
-packages = find_packages(), 
-# mention all the requirements you want
-# you want to create a function with a parth here since you might 
-# have 100s of requirements
-install_requires = get_requirements('requirements.py')
+    name = "mlproject",
+    version = '0.0.1',
+    author = 'Mizan',
+    author_email = "mizanur55@hotmail.com",
+    # this will install all the packages in from every folder with __init__ 
+    packages = find_packages(), 
+    # mention all the requirements you want
+    # you want to create a function with a parth here since you might 
+    # have 100s of requirements
+    install_requires = get_requirements('requirements.txt')
 )
